66ee3c33de0d38256c782761921017d8c19e8cfc
Gabriel Tiossi <gabrieltiossi@gmail.com>
2019-04-25 09:11:04 -0300

    Restructuring SimpleJob type to support v1.Containers natively

--- pkg/simplejob.go
+++ pkg/simplejob.go
@@ -1,15 +1,25 @@
 package pkg
 
+import (
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
 type SimpleJob struct {
-	Name       string      `json: "name"`
-	Containers []Container `json: "containers"`
-	Scheduled  bool        `json: "scheduled"`
-	MaxRetries int         `json: "maxRetries"`
-	Cron       string      `json: "cron,omitempty"`
+	Name       string         `json: "name"`
+	Namespace  string         `json: "namespace"`
+	Containers []v1.Container `json: "containers"`
+	Scheduled  bool           `json: "scheduled"`
+	MaxRetries int32          `json: "maxRetries"`
+	Cron       string         `json: "cron,omitempty"`
 }
 
-type Container struct {
-	Name    string   `json: "name"`
-	Image   string   `json: "image"`
-	Command []string `json: "command"`
+func (sj SimpleJob) CreateObjectMeta() metav1.ObjectMeta {
+	return metav1.ObjectMeta{
+		Name:      sj.Name,
+		Namespace: sj.Namespace,
+		Labels: map[string]string{
+			"job": sj.Name,
+		},
+	}
 }
