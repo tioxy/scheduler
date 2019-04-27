8f15f5cb33f7c2a6b56d326661c918b881855731
Gabriel Tiossi <gabrieltiossi@gmail.com>
2019-04-27 18:39:53 -0300

    Grouping & Organizing routes inside internal/app/main.go

--- internal/app/main.go
+++ internal/app/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	//scheduler "github.com/tioxy/scheduler/pkg"
 )
@@ -9,40 +11,54 @@ var err error
 
 func main() {
 	r := gin.Default()
+
 	r.GET("/", Root)
+	r.GET("/healthz", healthCheck)
+	r.GET("/metrics", exportMetrics)
 
-	r.GET("/jobs/", GetSimpleJobs)
-	r.POST("/jobs/:name", CreateSimpleJob)
-	r.GET("/jobs/:name", ReadSimpleJob)
-	r.PUT("/jobs/:name", UpdateSimpleJob)
-	r.DELETE("/jobs/:name", DeleteSimpleJob)
-
-	//r.GET("/healthz", ListSimpleJobs)
-	//r.GET("/metrics", ListSimpleJobs)
+	v1 := r.Group("/api/v1/jobs")
+	{
+		v1.GET("/", getSimpleJobs)
+		v1.POST("/:name", createSimpleJob)
+		v1.GET("/:name", fetchSimpleJob)
+		v1.PUT("/:name", updateSimpleJob)
+		v1.DELETE("/:name", deleteSimpleJob)
+	}
 
 	r.Run(":8080")
 }
 
 func Root(c *gin.Context) {
+	c.JSON(
+		http.StatusOK,
+		gin.H{"status": http.StatusOK, "message": "Scheduler Running :D"},
+	)
+}
+
+func getSimpleJobs(c *gin.Context) {
 
 }
 
-func GetSimpleJobs(c *gin.Context) {
+func createSimpleJob(c *gin.Context) {
 
 }
 
-func CreateSimpleJob(c *gin.Context) {
+func fetchSimpleJob(c *gin.Context) {
 
 }
 
-func ReadSimpleJob(c *gin.Context) {
+func deleteSimpleJob(c *gin.Context) {
 
 }
 
-func DeleteSimpleJob(c *gin.Context) {
+func updateSimpleJob(c *gin.Context) {
 
 }
 
-func UpdateSimpleJob(c *gin.Context) {
+func healthCheck(c *gin.Context) {
+
+}
+
+func exportMetrics(c *gin.Context) {
 
 }
